76d9fc2f1aa105898aeb6fd3ae2d86be86c8637a
Benjamin Hivert <ben.hivert@gmail.com>
2022-02-25 14:31:22 +0100

    Add files to reproduce Figure 2

---
+++ figures-codes/Figure2.R
@@ -0,0 +1,94 @@
+library(ggplot2)
+library(RColorBrewer)
+library(wesanderson)
+library(paletteer)
+library(patchwork)
+library(cowplot)
+library(dplyr)
+library(latex2exp)
+theme_set(theme_bw())
+source(file = "utils.R")
+
+pal <- wes_palette("Darjeeling1", 3, type = "continuous")
+pal2 <- c(wes_palette("GrandBudapest2", 2, type = "continuous"), "#cad3fa", pal[2])
+
+pal_illu <- c("#399283", "#e1d936", "#e09c6c", "#dc3c07")
+
+# Illustration
+
+# Parameters 
+n <- 200
+muH0 <- 0
+muH1X1 <- c(-5, 5, 0)
+muH1X2 <- c(0,0,10)
+
+set.seed(15112021)
+XH0 <- data.frame(X1 = rnorm(n, muH0), 
+                  X2 = rnorm(n, muH0))
+XH1 <- data.frame(X1 = c(rnorm(n/3, mean = muH1X1[1]),
+                         rnorm(n/3, mean = muH1X1[2]),
+                         rnorm(n/3, mean = muH1X1[3])),
+                  X2 = c(rnorm(n/3, mean = muH1X2[1]),
+                         rnorm(n/3, mean = muH1X2[2]),
+                         rnorm(n/3, mean =muH1X2[3])))
+X<- data.frame(rbind(XH0, XH1), 
+               Cluster = as.factor(c(hcl3(XH0), hcl3(XH1))),
+               Hypothesis = c(rep("H0", nrow(XH0)), 
+                              rep("H1", nrow(XH1))))
+
+pbehav_illu <- ggplot(X) + aes(x=X1, y = X2, colour = Cluster) +
+  geom_point() +
+  scale_colour_manual(values = c(pal_illu[1], pal_illu[4], pal_illu[3])) +
+  facet_wrap(~Hypothesis, scale = "free") +
+  theme(legend.position = "bottom",
+        axis.title = element_text(size = 14),
+        strip.text = element_text(size=12))
+pbehav_illu + ggtitle("Experience")
+
+# Results over 2 000 simulations of the data
+nsimu <- 2000
+
+pval_H0 <- read.csv(file = "simulations-results/simuH0.csv")
+pval_H1 <- read.csv(file = "simulations-results/simuH1.csv")
+
+pval_H0.df <- data.frame(pvalues = c(as.numeric(as.matrix(pval_H0))),
+                         Method = c(rep("Selective Test", 6*nsimu),
+                                    rep("Mulitmodality Test", 6*nsimu),
+                                    rep("t-test", 6*nsimu),
+                                    rep("Selective Test 2", 6*nsimu)),
+                         Variable = rep(c(rep("X1", 3*nsimu),
+                                          rep("X2", 3*nsimu)),4),
+                         Test = rep(c(rep("C1vsC2", nsimu),
+                                      rep("C1vsC3", nsimu),
+                                      rep("C2vsC3",nsimu)),8),
+                         Hypothesis = "H0")
+pval_H1.df <- data.frame(pvalues = c(as.numeric(as.matrix(pval_H1))),
+                         Method = c(rep("Selective Test", 6*nsimu),
+                                    rep("Mulitmodality Test", 6*nsimu),
+                                    rep("t-test", 6*nsimu),
+                                    rep("Selective Test 2", 6*nsimu)),
+                         Variable = rep(c(rep("X1", 3*nsimu),
+                                          rep("X2", 3*nsimu)),4),
+                         Test = rep(c(rep("C1vsC2", nsimu),
+                                      rep("C1vsC3", nsimu),
+                                      rep("C2vsC3",nsimu)),8),
+                         Hypothesis = "H1")
+pval_2D <- rbind(pval_H0.df, pval_H1.df)
+
+pbehav_res <- ggplot(pval_2D) + aes(x=Test, y = pvalues, colour = Method, fill = Method) +
+  geom_boxplot(alpha=.4) +
+  scale_discrete_manual(c("color", "fill"), values = pal2, labels = lapply(c("Multimodality test", 
+                                                                             r'(Selective test : $p_g^{C_k, C_l}$)',
+                                                                             r'(Selective test : $p_g^{C_k:C_l}$)',
+                                                                             "t-test"), TeX)) + 
+  facet_grid(Variable~Hypothesis)  +
+  ylab("p-values") +
+  theme(legend.position = "bottom",
+        axis.title = element_text(size = 14),
+        strip.text = element_text(size=12))
+pbehav_res +  ggtitle("Results for 2000 simualtions of the data")
+
+pbehav <- pbehav_illu / pbehav_res + 
+  plot_layout(heights = c(1, 2), widths = c(2,1)) + 
+  plot_annotation(tag_levels = 'A')  & theme(plot.tag = element_text(face = 'bold')) 
+ggsave(pbehav, file = "figures/figure2.pdf", dpi = 600, width = 155, height = 232.5, units = "mm")
---
+++ simulations-codes/behaviour_pval_2D.R
@@ -0,0 +1,121 @@
+# CURTA Simulation : Behaviour of p-values for two cases 
+
+
+# ID of task
+#slar_taskid <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
+
+# Packages 
+library(PCVI)
+
+# Function to be used
+hcl3 <- function(x){
+  return(cutree(hclust(dist(x), method = "ward.D"), k=3))
+}
+
+# Paramater 
+nsimu <- 2000
+n <- 200
+muH0 <- 0
+muH1X1 <- c(-5, 5, 0)
+muH1X2 <- c(0,0,10)
+
+
+# Save results
+pval_fig2H0 <- pval_fig2H1 <- matrix(NA, nrow = nsimu, ncol = 24)
+filename_rH0 <- paste0("result_r/simuH0", ".csv")
+filename_rH1 <- paste0("result_r/simuH1",".csv")
+
+
+for (i in 1:nsimu){
+  XH0 <- data.frame(X1 = rnorm(n, muH0), 
+                    X2 = rnorm(n, muH0))
+  XH1 <- data.frame(X1 = c(rnorm(n/3, mean = muH1X1[1]),
+                           rnorm(n/3, mean = muH1X1[2]),
+                           rnorm(n/3, mean = muH1X1[3])),
+                    X2 = c(rnorm(n/3, mean = muH1X2[1]),
+                           rnorm(n/3, mean = muH1X2[2]),
+                           rnorm(n/3, mean =muH1X2[3])))
+  clH0 <- hcl3(XH0)
+  clH1 <- hcl3(XH1)
+  
+  ## H0
+  # Selective test
+  pval_fig2H0[i,1] <- test_selective_inference(as.matrix(XH0), k1=1, k2=2, cl=clH0, cl_fun = hcl3, g=1, ndraws = 2000)$pval
+  pval_fig2H0[i,2] <- test_selective_inference(as.matrix(XH0), k1=1, k2=3, cl=clH0, cl_fun = hcl3, g=1, ndraws = 2000)$pval
+  pval_fig2H0[i,3] <- test_selective_inference(as.matrix(XH0), k1=2, k2=3, cl=clH0, cl_fun = hcl3, g=1, ndraws = 2000)$pval
+  
+  pval_fig2H0[i,4] <- test_selective_inference(as.matrix(XH0), k1=1, k2=2, cl=clH0, cl_fun = hcl3, g=2, ndraws = 2000)$pval
+  pval_fig2H0[i,5] <- test_selective_inference(as.matrix(XH0), k1=1, k2=3, cl=clH0, cl_fun = hcl3, g=2, ndraws = 2000)$pval
+  pval_fig2H0[i,6] <- test_selective_inference(as.matrix(XH0), k1=2, k2=3, cl=clH0, cl_fun = hcl3, g=2, ndraws = 2000)$pval
+  
+  #Multimodality test
+  pval_fig2H0[i, 7] <- test_multimod(as.matrix(XH0), g=1, cl = clH0, k1=1, k2=2)$pval
+  pval_fig2H0[i, 8] <- test_multimod(as.matrix(XH0), g=1, cl = clH0, k1=1, k2=3)$pval
+  pval_fig2H0[i, 9] <- test_multimod(as.matrix(XH0), g=1, cl = clH0, k1=2, k2=3)$pval
+  
+  pval_fig2H0[i, 10] <- test_multimod(as.matrix(XH0), g=2, cl = clH0, k1=1, k2=2)$pval
+  pval_fig2H0[i, 11] <- test_multimod(as.matrix(XH0), g=2, cl = clH0, k1=1, k2=3)$pval
+  pval_fig2H0[i, 12] <- test_multimod(as.matrix(XH0), g=2, cl = clH0, k1=2, k2=3)$pval
+  
+  #t-test
+  pval_fig2H0[i, 13] <- t.test(XH0[clH0==1,1], XH0[clH0==2,1])$p.value
+  pval_fig2H0[i, 14] <- t.test(XH0[clH0==1,1], XH0[clH0==3,1])$p.value
+  pval_fig2H0[i, 15] <- t.test(XH0[clH0==3,1], XH0[clH0==2,1])$p.value
+  
+  pval_fig2H0[i, 16] <- t.test(XH0[clH0==1,2], XH0[clH0==2,2])$p.value
+  pval_fig2H0[i, 17] <- t.test(XH0[clH0==1,2], XH0[clH0==3,2])$p.value
+  pval_fig2H0[i, 18] <- t.test(XH0[clH0==3,2], XH0[clH0==2,2])$p.value
+  
+  #Merging
+  pval_fig2H0[i, 19] <- merge_selective_inference(as.matrix(XH0), k1=1, k2=2, cl=clH0, cl_fun = hcl3, g=1, ndraws = 2000)$pval
+  pval_fig2H0[i, 20] <- merge_selective_inference(as.matrix(XH0), k1=1, k2=3, cl=clH0, cl_fun = hcl3, g=1, ndraws = 2000)$pval
+  pval_fig2H0[i, 21] <- merge_selective_inference(as.matrix(XH0), k1=2, k2=3, cl=clH0, cl_fun = hcl3, g=1, ndraws = 2000)$pval
+  
+  pval_fig2H0[i, 22] <- merge_selective_inference(as.matrix(XH0), k1=1, k2=2, cl=clH0, cl_fun = hcl3, g=2, ndraws = 2000)$pval
+  pval_fig2H0[i, 23] <- merge_selective_inference(as.matrix(XH0), k1=1, k2=3, cl=clH0, cl_fun = hcl3, g=2, ndraws = 2000)$pval
+  pval_fig2H0[i, 24] <- merge_selective_inference(as.matrix(XH0), k1=2, k2=3, cl=clH0, cl_fun = hcl3, g=2, ndraws = 2000)$pval
+  
+  ## H1
+  # Selective test
+  pval_fig2H1[i,1] <- test_selective_inference(as.matrix(XH1), k1=1, k2=2, cl=clH1, cl_fun = hcl3, g=1, ndraws = 2000)$pval
+  pval_fig2H1[i,2] <- test_selective_inference(as.matrix(XH1), k1=1, k2=3, cl=clH1, cl_fun = hcl3, g=1, ndraws = 2000)$pval
+  pval_fig2H1[i,3] <- test_selective_inference(as.matrix(XH1), k1=2, k2=3, cl=clH1, cl_fun = hcl3, g=1, ndraws = 2000)$pval
+  
+  pval_fig2H1[i,4] <- test_selective_inference(as.matrix(XH1), k1=1, k2=2, cl=clH1, cl_fun = hcl3, g=2, ndraws = 2000)$pval
+  pval_fig2H1[i,5] <- test_selective_inference(as.matrix(XH1), k1=1, k2=3, cl=clH1, cl_fun = hcl3, g=2, ndraws = 2000)$pval
+  pval_fig2H1[i,6] <- test_selective_inference(as.matrix(XH1), k1=2, k2=3, cl=clH1, cl_fun = hcl3, g=2, ndraws = 2000)$pval
+  
+  #Multimodality test
+  pval_fig2H1[i, 7] <- test_multimod(as.matrix(XH1), g=1, cl = clH1, k1=1, k2=2)$pval
+  pval_fig2H1[i, 8] <- test_multimod(as.matrix(XH1), g=1, cl = clH1, k1=1, k2=3)$pval
+  pval_fig2H1[i, 9] <- test_multimod(as.matrix(XH1), g=1, cl = clH1, k1=2, k2=3)$pval
+  
+  pval_fig2H1[i, 10] <- test_multimod(as.matrix(XH1), g=2, cl = clH1, k1=1, k2=2)$pval
+  pval_fig2H1[i, 11] <- test_multimod(as.matrix(XH1), g=2, cl = clH1, k1=1, k2=3)$pval
+  pval_fig2H1[i, 12] <- test_multimod(as.matrix(XH1), g=2, cl = clH1, k1=2, k2=3)$pval
+  
+  #t-test
+  pval_fig2H1[i, 13] <- t.test(XH1[clH1==1,1], XH1[clH1==2,1])$p.value
+  pval_fig2H1[i, 14] <- t.test(XH1[clH1==1,1], XH1[clH1==3,1])$p.value
+  pval_fig2H1[i, 15] <- t.test(XH1[clH1==3,1], XH1[clH1==2,1])$p.value
+  
+  pval_fig2H1[i, 16] <- t.test(XH1[clH1==1,2], XH1[clH1==2,2])$p.value
+  pval_fig2H1[i, 17] <- t.test(XH1[clH1==1,2], XH1[clH1==3,2])$p.value
+  pval_fig2H1[i, 18] <- t.test(XH1[clH1==3,2], XH1[clH1==2,2])$p.value
+  
+  #Merging
+  pval_fig2H1[i, 19] <- merge_selective_inference(as.matrix(XH1), k1=1, k2=2, cl=clH1, cl_fun = hcl3, g=1, ndraws = 2000)$pval
+  pval_fig2H1[i, 20] <- merge_selective_inference(as.matrix(XH1), k1=1, k2=3, cl=clH1, cl_fun = hcl3, g=1, ndraws = 2000)$pval
+  pval_fig2H1[i, 21] <- merge_selective_inference(as.matrix(XH1), k1=2, k2=3, cl=clH1, cl_fun = hcl3, g=1, ndraws = 2000)$pval
+  
+  pval_fig2H1[i, 22] <- merge_selective_inference(as.matrix(XH1), k1=1, k2=2, cl=clH1, cl_fun = hcl3, g=2, ndraws = 2000)$pval
+  pval_fig2H1[i, 23] <- merge_selective_inference(as.matrix(XH1), k1=1, k2=3, cl=clH1, cl_fun = hcl3, g=2, ndraws = 2000)$pval
+  pval_fig2H1[i, 24] <- merge_selective_inference(as.matrix(XH1), k1=2, k2=3, cl=clH1, cl_fun = hcl3, g=2, ndraws = 2000)$pval
+  write.csv(pval_fig2H0, file = filename_rH0, row.names = F)
+  write.csv(pval_fig2H1, file = filename_rH1, row.names = F)
+}
+
+
+
+
+
