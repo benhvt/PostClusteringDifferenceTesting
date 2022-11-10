55e2e3fe57a96de595c04b96b2dfb6bbbe482d40
Benjamin Hivert <ben.hivert@gmail.com>
2022-11-10 15:25:31 +0100

    Add HIPC Application

---
+++ application-codes/HIPC_data.R
@@ -0,0 +1,164 @@
+#--- HIPC data analysis ---#
+
+# install.packages("cytometree")
+
+library(ggplot2)
+library(patchwork)
+library(rstatix)
+library(ggpubr)
+library(VALIDICLUST)
+library(MetBrewer)
+library(cytometree)
+library(dplyr)
+library(kableExtra)
+source("utils.R")
+
+# Load data from the cytometree package
+data("HIPC")
+
+# Convert data to data frame and recode labels modality
+HIPC <- HIPC %>% data.frame() %>%
+  mutate(label = as.factor(label))
+HIPC$label <- forcats::fct_recode(HIPC$label,"CD8 Effector" = "1",
+                                  "CD8 Naive" = "2",
+                                  "CD8 Central Memory"= "3",
+                                  "CD8 Effector Memory" = "4",
+                                  "CD8 Activated" = "5",
+                                  "CD4 Effector" = "6",
+                                  "CD4 Naive" = "7",
+                                  "CD4 Central Memory" = "8", 
+                                  "CD4 Effector Memory" = "9", 
+                                  "CD4 Activated" = "10")
+
+# Keep only the cells in one of the following population: "CD8 Naive", "CD8 Effector Memory", "CD4 Naive", "CD4 Effector Memory")
+HIPC_samp <- HIPC %>%
+  filter(label %in% c("CD8 Naive", "CD8 Effector Memory", "CD4 Naive", "CD4 Effector Memory"))
+HIPC_samp$label <- droplevels(HIPC_samp$label)
+
+
+# Keep only 5% of cells of each population
+set.seed(2102022)
+prop <- 0.05
+HIPC_sub <- HIPC_samp %>%
+  group_by(label) %>% 
+  slice_sample(prop = prop)
+
+# Clustering step (on scaled data)
+HIPC_scale <- HIPC_sub %>%
+  ungroup() %>%
+  dplyr:: select(-c(label)) %>% 
+  scale()
+
+## Define function for clustering 
+cl_fun <- function(x){cutree(fastcluster::hclust(dist(x), method = "ward.D2"), k=4)}
+
+clust <- as.factor(cl_fun(HIPC_scale))
+
+## Comparaison with the ground truth 
+ARI <- mclust::adjustedRandIndex(HIPC_sub$label, clust)
+
+# Inference step:
+## Test on cluster 1
+C1C2 <- apply3test(HIPC_scale, cl_fun = cl_fun, cl = clust, k1=1, k2=2)
+C1C3 <- apply3test(HIPC_scale, cl_fun = cl_fun, cl = clust, k1=1, k2=3)
+C1C4 <- apply3test(HIPC_scale, cl_fun = cl_fun, cl = clust, k1=1, k2=4)
+
+## Test on cluster 2
+C2C3 <- apply3test(HIPC_scale, cl_fun = cl_fun, cl = clust, k1=2, k2=3)
+C2C4 <- apply3test(HIPC_scale, cl_fun = cl_fun, cl = clust, k1=2, k2=4)
+
+## Test on cluster 3  
+C3C4 <- apply3test(HIPC_scale, cl_fun = cl_fun, cl = clust, k1=3, k2=4)
+
+
+# Results 
+
+## Main Results 
+
+### Figures
+plt_multimodality_test_res <- box_pval(HIPC_scale, test = "Multimod", var = "CCR7", clust = clust) + 
+  box_pval(HIPC_scale, test = "Multimod", var = "CD4", clust = clust)+
+  box_pval(HIPC_scale, test = "Multimod", var = "CD45RA", clust = clust) +
+  box_pval(HIPC_scale, test = "Multimod", var = "HLADR", clust = clust) +
+  box_pval(HIPC_scale, test = "Multimod", var = "CD38", clust = clust) +
+  box_pval(HIPC_scale, test = "Multimod", var = "CD8", clust = clust) +
+  plot_layout(guides = "collect") &
+  theme(legend.position = "bottom", 
+        strip.text = element_text(size = 18),
+        axis.title = element_text(size = 16),
+        axis.text = element_text(size = 16),
+        legend.title = element_text(size = 18),
+        legend.text = element_text(size = 16))
+ggsave(plt_multimodality_test_res, filename = "figures/figure6.pdf",
+       width = 300, height = 200, units = "mm")
+
+ plt_multimodality_test_res
+### Table
+rownames(C1C2$pval) <- rownames(C1C3$pval) <- rownames(C1C4$pval) <- rownames(C2C3$pval) <- rownames(C2C4$pval) <- rownames(C3C4$pval) <- colnames(HIPC_scale)
+
+colnames(C1C2$pval) <- colnames(C1C3$pval) <- colnames(C1C4$pval) <- colnames(C2C3$pval) <- colnames(C2C4$pval) <- colnames(C3C4$pval) <- c("Selective test", "Merging test", "Mutlimodality test")
+restab <- table(clust, HIPC_sub$label)
+proptab <- prop.table(restab, margin = 1)
+C2C4$pval %>% round(4) %>% 
+  xtable::xtable(digits = 4,
+                 caption = paste("Comparison between Cluster 2 (", 
+                                 round(proptab[2,"CD8 Effector Memory"],2)*100, 
+                                 "% of CD8 Effector Memory cells) and Cluster 4 (",
+                                 round(proptab[4,"CD4 Effector Memory"],2)*100,"% of CD4 Effector Memory cells)", sep = ""), 
+                 label = "tab:resC2C4HIPC")
+
+## Supplementary results 
+
+### Supplementary table
+
+pval_results <- data.frame(Markers = rep(colnames(HIPC_scale), 6))
+pval_results$`Selective test` <- c(C1C2$pval[,1],
+                                   C1C3$pval[,1],
+                                   C1C4$pval[,1],
+                                   C2C3$pval[,1],
+                                   C2C4$pval[,1],
+                                   C3C4$pval[,1])
+
+pval_results$`Merging test` <- c(C1C2$pval[,2],
+                                 C1C3$pval[,2],
+                                 C1C4$pval[,2],
+                                 C2C3$pval[,2],
+                                 C2C4$pval[,2],
+                                 C3C4$pval[,2])
+
+pval_results$`Multimodality test` <- c(C1C2$pval[,3],
+                                       C1C3$pval[,3],
+                                       C1C4$pval[,3],
+                                       C2C3$pval[,3],
+                                       C2C4$pval[,3],
+                                       C3C4$pval[,3])
+
+
+add_percentage_ground_truth <- function(cluster, clust1, clust2, label, lab1, lab2){
+  restab <- table(cluster, label)
+  proptab <- prop.table(restab, margin = 1)
+  return(paste("Cluster", clust1, " (", 
+               round(proptab[clust1, lab1],2)*100, 
+               "% of ", lab1, ") vs Cluster", clust2, " (",
+               round(proptab[clust2,lab2],2)*100,"% of ", lab2, ")", sep = ""))
+}
+
+
+pval_results %>% 
+  mutate(`Selective test` = round(`Selective test`,4)) %>%
+  mutate(`Merging test` = round(`Merging test`, 4)) %>%
+  mutate(`Multimodality test` = round(`Multimodality test`,4)) %>%
+  mutate(`Selective test`, `Selective test` = ifelse(`Selective test`<0.05, paste(`Selective test`, "*", sep = ""),`Selective test`)) %>%
+  mutate(`Merging test`, `Merging test` = ifelse(`Merging test`<0.05, paste(`Merging test`, "*", sep = ""), `Merging test`)) %>%
+  mutate(`Multimodality test`, `Multimodality test` = ifelse(`Multimodality test`<0.05, paste(`Multimodality test`, "*", sep = ""), `Multimodality test`)) %>%
+  kbl(digits = 4, booktabs = T, longtable = T) %>%
+  kable_styling(font_size = 30) %>%
+  kable_classic(full_width = T, html_font = "Cambria") %>%
+  row_spec(0,bold=TRUE) %>%
+  pack_rows(add_percentage_ground_truth(cluster = clust, clust1 = 1, clust2 = 2, label = HIPC_sub$label, lab1 = "CD8 Naive", lab2 = "CD8 Effector Memory"), 1, 6) %>%
+  pack_rows(add_percentage_ground_truth(cluster = clust, clust1 = 1, clust2 = 3, label = HIPC_sub$label, lab1 = "CD8 Naive", lab2 = "CD4 Naive"), 7, 12) %>%
+  pack_rows(add_percentage_ground_truth(cluster = clust, clust1 = 1, clust2 = 4, label = HIPC_sub$label, lab1 = "CD8 Naive", lab2 = "CD4 Effector Memory"), 13, 18) %>%
+  pack_rows(add_percentage_ground_truth(cluster = clust, clust1 = 2, clust2 = 3, label = HIPC_sub$label, lab1 = "CD8 Effector Memory", lab2 = "CD4 Naive"), 19, 24) %>%
+  pack_rows(add_percentage_ground_truth(cluster = clust, clust1 = 2, clust2 = 4, label = HIPC_sub$label, lab1 = "CD8 Effector Memory", lab2 = "CD4 Effector Memory"), 25, 30) %>%
+  pack_rows(add_percentage_ground_truth(cluster = clust, clust1 = 3, clust2 = 4, label = HIPC_sub$label, lab1 = "CD4 Naive", lab2 = "CD4 Effector Memory"), 31, 36) %>%
+  kable_styling()
--- utils.R
+++ utils.R
@@ -31,3 +31,116 @@ hcl4 <- function(x){
   cl <- factor(cl, labels = 1:4)
   return(as.factor(cl))
 }
+
+
+apply3test <- function(x, cl_fun, cl, k1, k2, sig = NULL){
+  # Apply our three proposed test for a pair of clusters on all the variables of the data 
+  pval <- matrix(NA, nrow = ncol(x), ncol = 3)
+  if (is.null(sig)){
+    for (j in 1:ncol(x)){
+      pval[j,1] <- VALIDICLUST::test_selective_inference(x,
+                                                         g=j,
+                                                         k1 = k1,
+                                                         k2 = k2,
+                                                         cl = cl,
+                                                         cl_fun = cl_fun,
+                                                         ndraws = 2000)$pval
+      xg <- data.frame(Xg=x[,j], Cluster = cl)
+      Ck1k2 <- xg %>% group_by(Cluster) %>% summarise(Mean = mean(Xg)) %>% arrange(Mean)
+      adjacent <- as.numeric(Ck1k2$Cluster[which(Ck1k2$Cluster == k1):which(Ck1k2$Cluster == k2)])
+      if (length(adjacent) == 2){
+        pval[j,2] <- pval[j,1]
+      }
+      else{
+        pval[j,2] <- VALIDICLUST::merge_selective_inference(x,
+                                                            g=j,
+                                                            k1 = k1,
+                                                            k2 = k2,
+                                                            cl = cl,
+                                                            cl_fun = cl_fun,
+                                                            ndraws = 2000)$pval
+      }
+      pval[j,3] <- VALIDICLUST::test_multimod(x,
+                                              g=j,
+                                              k1=k1,
+                                              k2=k2,
+                                              cl = cl
+      )$pval
+    }
+  }
+  else {
+    for (j in 1:ncol(x)){
+      pval[j,1] <- VALIDICLUST::test_selective_inference(x,
+                                                         g=j,
+                                                         k1 = k1,
+                                                         k2 = k2,
+                                                         cl = cl,
+                                                         cl_fun = cl_fun,
+                                                         ndraws = 2000,
+                                                         sig = sd(x[,j]))$pval
+      
+      xg <- data.frame(Xg=x[,j], Cluster = cl)
+      Ck1k2 <- xg %>% group_by(Cluster) %>% summarise(Mean = mean(Xg)) %>% arrange(Mean)
+      adjacent <- as.numeric(Ck1k2$Cluster[which(Ck1k2$Cluster == k1):which(Ck1k2$Cluster == k2)])
+      if (length(adjacent) == 2){
+        pval[j,2] <- pval[j,1]
+      }
+      else{
+        pval[j,2] <- VALIDICLUST::merge_selective_inference(x,
+                                                            g=j,
+                                                            k1 = k1,
+                                                            k2 = k2,
+                                                            cl = cl,
+                                                            cl_fun = cl_fun,
+                                                            ndraws = 2000)$pval
+      }
+      
+      pval[j,3] <- VALIDICLUST::test_multimod(x,
+                                              g=j,
+                                              k1=k1,
+                                              k2=k2,
+                                              cl = cl
+      )$pval
+    }
+  }
+  pval.adj <- apply(pval, 2, function(x){p.adjust(x, method = "BH")})
+  return(list(pval = pval, pval.adj = pval.adj))
+}
+
+box_pval <- function(data, test, var, clust, add = "sig"){
+  # Function to plot boxplot with p-values
+  bxp <- data.frame(data) %>% 
+    mutate(Cluster = clust) %>% 
+    ggboxplot(x="Cluster", y = var, fill = "Cluster") +
+    scale_colour_met_d("Hokusai1") +
+    scale_fill_met_d("Hokusai1") +
+    theme(strip.text = element_text(size = 16),
+          axis.title = element_text(size = 14),
+          axis.text = element_text(size = 14))
+  formule <- as.formula(paste(var, "Cluster", sep = "~"))
+  stat.test <- data.frame(data) %>% 
+    mutate(Cluster = clust) %>%
+    t_test(formule) %>%
+    add_significance()
+  ind.test <- ifelse(test == "SI", 1, ifelse(test == "Merge", 2, 3))
+  ind.var <- which(var == colnames(data))
+  stat.test$p.adj <- c(C1C2$pval[ind.var,ind.test],
+                       C1C3$pval[ind.var,ind.test],
+                       C1C4$pval[ind.var,ind.test],
+                       C2C3$pval[ind.var,ind.test],
+                       C2C4$pval[ind.var,ind.test],
+                       C3C4$pval[ind.var,ind.test])
+  
+  stat.test$p.adj.signif <- ifelse(stat.test$p.adj<0.01, "**", 
+                                   ifelse(stat.test$p.adj>=0.01 & stat.test$p.adj<0.05, "*", "ns"))
+  
+  stat.test <- stat.test %>% add_xy_position(x = "Cluster")
+  if (add == "pval"){
+    plt <- bxp + stat_pvalue_manual(stat.test, label = "p = {round(p.adj,3)}")
+  }
+  else {
+    plt <- bxp + stat_pvalue_manual(stat.test, label = "p.adj.signif", label.size = 8, size = 8, hide.ns = T)
+  }
+  return(plt)
+  
+}
